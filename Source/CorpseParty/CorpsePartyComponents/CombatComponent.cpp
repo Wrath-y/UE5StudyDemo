03e326778d80d21f3fa1d8601eac047e1d7e3275
Wrath-y <805655528@qq.com>
2023-11-30 21:07:31 +0800

    feat: 添加换弹音效 换弹时不应用AimOffset

--- Source/CorpseParty/CorpsePartyComponents/CombatComponent.cpp
+++ Source/CorpseParty/CorpsePartyComponents/CombatComponent.cpp
@@ -13,6 +13,7 @@
 #include "CorpseParty/HUD/CorpsePartyHUD.h"
 #include "CorpseParty/PlayerController/CorpsePartyPlayerController.h"
 #include "Camera/CameraComponent.h"
+#include "Sound/SoundCue.h"
 
 UCombatComponent::UCombatComponent()
 {
@@ -159,6 +160,15 @@ void UCombatComponent::EquipWeapon(AWeapon* WeaponToEquip)
 	{
 		Controller->SetHUDCarriedAmmo(CarriedAmmo);
 	}
+
+	if (EquippedWeapon->EquipSound)
+	{
+		UGameplayStatics::PlaySoundAtLocation(
+			this,
+			EquippedWeapon->EquipSound,
+			Character->GetActorLocation()
+		);
+	}
 	
 	Character->GetCharacterMovement()->bOrientRotationToMovement = false;
 	Character->bUseControllerRotationYaw = true;
@@ -190,6 +200,16 @@ void UCombatComponent::OnRep_EquippedWeapon()
 		}
 		Character->GetCharacterMovement()->bOrientRotationToMovement = false;
 		Character->bUseControllerRotationYaw = true;
+
+		
+		if (EquippedWeapon->EquipSound)
+		{
+			UGameplayStatics::PlaySoundAtLocation(
+				this,
+				EquippedWeapon->EquipSound,
+				Character->GetActorLocation()
+			);
+		}
 	}
 }
 
