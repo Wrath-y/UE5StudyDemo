eff20bd627d15f5ea9609fa95fc20e3a82167068
Wrath-y <805655528@qq.com>
2023-12-09 22:18:13 +0800

    feat: 添加狙击步枪

--- Source/CorpseParty/Character/CorpsePartyCharacter.cpp
+++ Source/CorpseParty/Character/CorpsePartyCharacter.cpp
@@ -289,6 +289,9 @@ void ACorpsePartyCharacter::PlayReloadMontage()
 		case EWeaponType::EWT_Shotgun:
 			SectionName = FName("Rifle");
 			break;
+		case EWeaponType::EWT_SniperRifle:
+			SectionName = FName("Rifle");
+			break;
 		}
 
 		AnimInstance->Montage_JumpToSection(SectionName);
--- Source/CorpseParty/CorpsePartyComponents/CombatComponent.h
+++ Source/CorpseParty/CorpsePartyComponents/CombatComponent.h
@@ -140,6 +140,9 @@ private:
 	
 	UPROPERTY(EditAnywhere)
 	int32 StartingShotgunAmmo = 0;
+
+	UPROPERTY(EditAnywhere)
+	int32 StartingSniperAmmo = 0;
 	
 	void InitializeCarriedAmmo();
 
