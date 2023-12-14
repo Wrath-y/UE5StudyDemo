60dacdd8c3f4f29b2c53a21f66aa17304e076bdb
Wrath-y <805655528@qq.com>
2023-12-14 18:26:44 +0800

    feat: 添加健康拾取器类与蓝图

---
+++ Source/CorpseParty/Pickups/HealthPickup.cpp
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HealthPickup.h"
+#include "CorpseParty/Character/CorpsePartyCharacter.h"
+#include "CorpseParty/CorpsePartyComponents/BuffComponent.h"
+#include "NiagaraFunctionLibrary.h"
+#include "NiagaraComponent.h"
+
+AHealthPickup::AHealthPickup()
+{
+	bReplicates = true;
+	PickupEffectComponent = CreateDefaultSubobject<UNiagaraComponent>(TEXT("PickupEffectComponent"));
+	PickupEffectComponent->SetupAttachment(RootComponent);
+
+}
+
+void AHealthPickup::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	Super::OnSphereOverlap(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex, bFromSweep, SweepResult);
+
+	ACorpsePartyCharacter* CorpsePartyCharacter = Cast<ACorpsePartyCharacter>(OtherActor);
+	if (CorpsePartyCharacter)
+	{
+
+	}
+
+	Destroy();
+}
+
+void AHealthPickup::Destroyed()
+{
+	if (PickupEffect)
+	{
+		UNiagaraFunctionLibrary::SpawnSystemAtLocation(
+			this,
+			PickupEffect,
+			GetActorLocation(),
+			GetActorRotation()
+		);
+	}
+	Super::Destroyed();
+}
