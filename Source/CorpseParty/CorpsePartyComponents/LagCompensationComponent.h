39394abdf2296c4da31274748ee54257006cb587
Wrath-y <805655528@qq.com>
2023-12-25 21:43:19 +0800

    feat: 完善服务端倒带算法

--- Source/CorpseParty/CorpsePartyComponents/LagCompensationComponent.h
+++ Source/CorpseParty/CorpsePartyComponents/LagCompensationComponent.h
@@ -33,6 +33,18 @@ struct FFramePackage
 	TMap<FName, FBoxInformation> HitBoxInfo;
 };
 
+USTRUCT(BlueprintType)
+struct FServerSideRewindResult
+{
+	GENERATED_BODY()
+
+	UPROPERTY()
+	bool bHitConfirmed;
+
+	UPROPERTY()
+	bool bHeadShot;
+};
+
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class CORPSEPARTY_API ULagCompensationComponent : public UActorComponent
 {
@@ -43,17 +55,26 @@ public:
 	friend class ACorpsePartyCharacter;
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 	void ShowFramePackage(const FFramePackage& Package, const FColor& Color);
-	void ServerSideRewind(
+	FServerSideRewindResult ServerSideRewind(
 		class ACorpsePartyCharacter* HitCharacter, 
 		const FVector_NetQuantize& TraceStart, 
 		const FVector_NetQuantize& HitLocation, 
 		float HitTime);
-	FFramePackage InterpBetweenFrames(const FFramePackage& OlderFrame, const FFramePackage& YoungerFrame, float HitTime);
 	
 protected:
 	virtual void BeginPlay() override;
 	void SaveFramePackage(FFramePackage& Package);
-
+	FFramePackage InterpBetweenFrames(const FFramePackage& OlderFrame, const FFramePackage& YoungerFrame, float HitTime);
+	FServerSideRewindResult ConfirmHit(
+			const FFramePackage& Package, 
+			ACorpsePartyCharacter* HitCharacter, 
+			const FVector_NetQuantize& TraceStart, 
+			const FVector_NetQuantize& HitLocation);
+	void CacheBoxPositions(ACorpsePartyCharacter* HitCharacter, FFramePackage& OutFramePackage);
+	void MoveBoxes(ACorpsePartyCharacter* HitCharacter, const FFramePackage& Package);
+	void ResetHitBoxes(ACorpsePartyCharacter* HitCharacter, const FFramePackage& Package);
+	void EnableCharacterMeshCollision(ACorpsePartyCharacter* HitCharacter, ECollisionEnabled::Type CollisionEnabled);
+	
 private:
 
 	UPROPERTY()
